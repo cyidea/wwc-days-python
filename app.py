165f5fea3a650866efb3900f062267c84d5c14e6
clarayuan <c_w_yuan@hotmail.com>
2024-01-11 17:16:47 -0600

    exercise 1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+# weight converter exercise:
+weight = input('Weight: ') #enter weight
+option = input('(K)g or (L)bs: ') # indicate if its kg or lb
+# convert the weight to the other option:
+weight = float(weight)
+if option.upper() == 'K':
+    weightLB = weight / 0.45
+    print('Weight in Lb:', weightLB)
+elif option.upper() == 'L':
+    weightKg = weight * 0.45
+    print('Weight in Kg:', weightKg)
