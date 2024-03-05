2730cc76540099b836eb0b40fe11f6fe91b5e490
clarayuan <c_w_yuan@hotmail.com>
2024-03-05 14:48:52 -0600

    add spaceship to alien invasion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlientInvasion:
     """Overall cls to manage game assets and behavior."""
@@ -16,19 +17,28 @@ class AlientInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """ start the main loop for the game. """
         while True:
-            # watch for the keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self.__check_events()
 
             # redraw the screen during each pass through the loop.
+            self.__update_screen()
+            self.clock.tick(60)
+
+    def __check_events(self):
+        """ watch for the keyboard and mouse events. """
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+    def __update_screen(self):
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
-            self.clock.tick(60)
 
 if __name__ == '__main__':
     # make a game instance, and run the game.
