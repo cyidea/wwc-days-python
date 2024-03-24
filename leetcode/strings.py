ebd45af956b3a0f10af0f0fd318d34a1430265b1
clarayuan <c_w_yuan@hotmail.com>
2024-03-24 18:59:22 -0500

    add a new leetcode problem. although with a bug

--- leetcode/strings.py
+++ leetcode/strings.py
@@ -14,10 +14,64 @@ class Solution:
     """
     def romanToInt(self, s: str) -> int:
         print(f'{self.dic}')
-        for c in s:
-            print(c)
-        return 0
+        
+        r = 0
+        rest_string = s
+        while rest_string:
+            print(rest_string)
+            c = rest_string[0]
+            the_next = False
+            if len(rest_string) > 1:
+                the_next = rest_string[1]
+            if c == 'I':
+                if the_next:
+                    if the_next == 'I':
+                        r += 1
+                    elif the_next == 'V':
+                        r += 4
+                        rest_string = rest_string[2:]
+                        continue
+                    elif the_next == 'X':
+                        r += 9
+                        rest_string = rest_string[2:]
+                        continue
+                else:
+                    r += 1
+            elif c == 'X':
+                if the_next:
+                    
+                    if the_next == 'L':
+                        r += 40
+                        rest_string = rest_string[2:]
+                        continue
+                    elif the_next == 'C':
+                        r += 90
+                        rest_string = rest_string[2:]
+                        continue
+            elif c== 'C':
+                if the_next:
+                    
+                    if the_next == 'D':
+                        r += 400
+                        rest_string = rest_string[2:]
+                        continue
+                    elif the_next == 'M':
+                        r += 900
+                        rest_string = rest_string[2:]
+                        continue
+            # special cases ended here
+            elif c == 'V':
+                r += 5
+            elif c == 'L':
+                r += 50
+            elif c == 'D':
+                r += 500
+            elif c == 'M':
+                r += 1000
+            rest_string = rest_string[1:]
+
+        return r
 
 s = Solution()
-ret = s.romanToInt("III")
+ret = s.romanToInt("MCMXCIV")
 print(f'{ret}')
