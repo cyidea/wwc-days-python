398c5ef4ca0f2a5eaada6c14e2dcec731c7b410b
clarayuan <c_w_yuan@hotmail.com>
2024-04-02 14:07:25 -0500

    change some wording

--- kaggle/weather_data_predict/weather.py
+++ kaggle/weather_data_predict/weather.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeRegressor
 # pip3 install pandas
 # pip3 install scikit-learn
 
-# Create a sample dataframe
+# Create the dataframe that contain historical weather data
 data = {'Temperature': [30, 32, 35, 20, 22, 25, 28],
         'Humidity': [60, 65, 70, 75, 80, 85, 90],
         'Pressure': [1010, 1012, 1015, 1008, 1006, 1003, 1000],
@@ -19,6 +19,8 @@ data = {'Temperature': [30, 32, 35, 20, 22, 25, 28],
 df = pd.DataFrame(data)
 
 # Separate features(X) and target(y) variables
+# We are singling out the AQI (air quality index)
+# because we want to predict AQI
 X = df.drop('AQI', axis=1)
 y = df['AQI']
 
@@ -26,7 +28,7 @@ y = df['AQI']
 regressor = DecisionTreeRegressor()
 regressor.fit(X, y)
 
-# Make predictions
+# Make predictions on new data
 new_data = {'Temperature': [27, 29],
             'Humidity': [70, 75],
             'Pressure': [1013, 1011],
@@ -35,7 +37,7 @@ new_data = {'Temperature': [27, 29],
             'Visibility': [8, 7]}
 new_df = pd.DataFrame(new_data)
 
-# Predict on new data
+# Predict on new data and store the result in 'predictions'
 predictions = regressor.predict(new_df)
 
 # Add predictions as a new column to the new_df
@@ -45,3 +47,18 @@ new_df['Predicted_AQI'] = predictions
 df_with_predictions = pd.concat([df, new_df], ignore_index=True)
 
 print(df_with_predictions)
+
+"""
+result: As you can see the predicted_AQI shows only for the new data
+   Temperature  Humidity  Pressure  WindSpeed  Rainfall  Visibility   AQI  Predicted_AQI
+0           30        60      1010          5         0          10  20.0            NaN
+1           32        65      1012          6         0           8  25.0            NaN
+2           35        70      1015          7         0           9  30.0            NaN
+3           20        75      1008          4         1           6  40.0            NaN
+4           22        80      1006          3         1           7  35.0            NaN
+5           25        85      1003          2         1           5  45.0            NaN
+6           28        90      1000          1         0          10  50.0            NaN
+7           27        70      1013          3         0           8   NaN           25.0
+8           29        75      1011          4         1           7   NaN           35.0
+
+"""
