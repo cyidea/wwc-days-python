f59f77574ab4f8d4f8c28ca28e5fbacb5b899e94
clarayuan <c_w_yuan@hotmail.com>
2024-04-18 09:41:25 -0500

    modify random forest with comments

--- kaggle/housing_data_fitting/housing_random_forest_pipeline.py
+++ kaggle/housing_data_fitting/housing_random_forest_pipeline.py
@@ -4,6 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+#todo: modify the following code to use pipelines, see kaggle example in intermediate machine learning
+
 
 # Load the ames_housing dataset and create the DataFrame
 ames_housing = fetch_openml(name="house_prices", as_frame=True)
@@ -14,8 +18,7 @@ df['target'] = ames_housing.target
 X = df.drop('target', axis=1)
 y = df['target']
 
-# try to prevent 'could not convert string to float' error.
-# the error occurs if there are string type values in the dataset
+# try to prevent 'could not convert string to float ' error:
 X = X.apply(pd.to_numeric, errors='coerce')
 y = y.apply(pd.to_numeric, errors='coerce')
 
@@ -45,32 +48,22 @@ result:
 MAE: 18316.85
 """
 
-# next we are going to get rid of the missing values
-# we first will deal with columns that have all columns missing
-# then we will 'Impute' the missing values with the 'median' of available values
+# RandomForestRegressor does seem to be an improvedment from
+# our last try with DecisionTreeRegressor
+# can we still improve it?
+# here are the parameters we can tweek: 
+# n_estimators, criterion='absolute_error', min_samples_split=20, max_depth=7
 
 print('The columns that are missing all values: ')
 cols_with_all_vals_missing = [col for col in X_train.columns
                      if X_train[col].isnull().all()]
 print(cols_with_all_vals_missing)
-"""
-Print out: Here are the columns we will drop:
-['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 
-'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 
-'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 
-'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
-'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 
-'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 
-'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
-"""
 
 # drop all these columns that have no value:
 X_train = X_train.drop(cols_with_all_vals_missing, axis=1)
 X_test = X_test.drop(cols_with_all_vals_missing, axis=1)
 
-# and we will use SimpleImputer that can deal with columns that have
-# both null values and non-null values
-# it will fill the null values with the median of existing values
+# and there is the SimpleImputer that can deal with null values
 my_imputer = SimpleImputer(strategy='median')
 imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
 imputed_X_test = pd.DataFrame(my_imputer.transform(X_test))
@@ -82,7 +75,7 @@ imputed_X_test.columns = X_test.columns
 # Create a RandomForestRegressor with random_state=0
 model = RandomForestRegressor(random_state=0)
 
-# Fit the model on the modified (imputed) training data
+# Fit the model on the training data
 model.fit(imputed_X_train, y_train)
 
 # Make predictions on the test data
