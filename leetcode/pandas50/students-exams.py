7146f17d702306341cdf064d78feb62d88b336cc
clarayuan <c_w_yuan@hotmail.com>
2024-06-15 17:23:55 -0400

    add students exam leetcode problem

--- leetcode/pandas50/students-exams.py
+++ leetcode/pandas50/students-exams.py
@@ -17,6 +17,34 @@ subjects_data = [
 ]
 subjects = pd.DataFrame(subjects_data, columns=['subject_name'])
 
+# Creating a cartesian product dataframe between student_id and subject_name
+student_id_list = students['student_id']
+subject_name_list = subjects['subject_name']
+
+newindex = pd.MultiIndex.from_product([student_id_list, subject_name_list], 
+                                   names = ["student_id", "subject_name"])
+
+allrows = pd.DataFrame(index = newindex).reset_index()
+print(allrows)
+
+'''
+    student_id subject_name
+0            1         Math
+1            1      Physics
+2            1  Programming
+3            2         Math
+4            2      Physics
+5            2  Programming
+6           13         Math
+7           13      Physics
+8           13  Programming
+9            6         Math
+10           6      Physics
+11           6  Programming
+'''
+
+
+
 # Examinations DataFrame
 examinations_data = [
     [1, 'Math'],
@@ -33,22 +61,15 @@ examinations_data = [
 ]
 examinations = pd.DataFrame(examinations_data, columns=['student_id', 'subject_name'])
 
-print("Students DataFrame:")
-print(students)
-print("\nSubjects DataFrame:")
-print(subjects)
-print("\nExaminations DataFrame:")
-print(examinations)
-
-
-# result = employees.merge(right=employeeUNI, left_on="id", right_on="employee_id", how="left")
-# print(result[['unique_id', 'name']])
-
-# result = examinations.groupby(['student_id','subject_name']).size().reset_index(name='count')
-
 result = examinations.groupby(['student_id','subject_name']).size().reset_index(name='attended_exams')
 
-merged = result.merge(right=students, on='student_id', how='outer').sort_values(by=['student_id', 'subject_name'])
+print('\n')
+print(result)
+print('\n')
+print('final result: ')
+merged = result.merge(right=allrows, on=['student_id', 'subject_name'], how='right').sort_values(by=['student_id', 'subject_name']).fillna(0)
+merged = merged.merge(right=students, on='student_id', how='left')
 
-print(merged)
+print(merged[['student_id', 'student_name', 'subject_name', 'attended_exams']])
+
 
